77ce0b8304aff684dfa6d8a20e1e6b7117c6d902
Aaron Hampton <aaron.hampt@gmail.com>
2015-09-24 01:43:27 +0000

    Adding initial files and updating license

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='mwslookup',
+    packages=['mwslookup'],
+    version='0.0.1',
+    description="Get Amazon product details from the MWS API",
+    author="Aaron Hampton",
+    author_email="aaron.hampt@gmail.com",
+    url="https://github.com/hamptus/mwslookup",
+)
