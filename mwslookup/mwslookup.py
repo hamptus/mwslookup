7b8e2cc1cb8149c0612eb1aac86c8ad0cff79337
Aaron Hampton <aaron.hampt@gmail.com>
2015-10-02 02:30:25 +0000

    Fixed IndexError and updated script to get competitive price

--- mwslookup/mwslookup.py
+++ mwslookup/mwslookup.py
@@ -13,14 +13,27 @@ def new_conn():
     )
     return conn
 
+def get_competitive(asin, condition='New'):
+    conn = new_conn()
+    resp = conn.get_competitive_pricing_for_asin(MarketplaceId=MarketplaceId, ASINList=[asin])
+    try:
+        sales_rank = resp.GetCompetitivePricingForASINResult[0].Product.SalesRankings.SalesRank[0].Rank
+    except IndexError:
+        sales_rank = 0
+    for i in resp.GetCompetitivePricingForASINResult[0].Product.CompetitivePricing[0].CompetitivePrices.CompetitivePrice:
+        if i['subcondition'] == condition:
+            return i.Price.LandedPrice.Amount, sales_rank
+
 def lookup(title):
     conn = new_conn()
     resp = conn.list_matching_products(MarketplaceId=MarketplaceId, Query=title)
-    p = resp.ListMatchingProductsResult.Products.Product[0]
+    try:
+        p = resp.ListMatchingProductsResult.Products.Product[0]
+    except IndexError:
+        return {}
 
     return_data = {}
     return_data['title'] = p.AttributeSets.ItemAttributes[0].Title
-    return_data['list_price'] = p.AttributeSets.ItemAttributes[0].ListPrice.Amount
     return_data['length'] = p.AttributeSets.ItemAttributes[0].PackageDimensions.Length.Value
     return_data['width'] = p.AttributeSets.ItemAttributes[0].PackageDimensions.Width.Value
     return_data['height'] = p.AttributeSets.ItemAttributes[0].PackageDimensions.Height.Value
@@ -28,4 +41,5 @@ def lookup(title):
 
     #         return_data['fees'] = calculate_fees(return_data['length'], return_data['width'], return_data['height'], return_data['weight'])
     return_data['asin'] = p.Identifiers.MarketplaceASIN.ASIN
+
     return return_data
